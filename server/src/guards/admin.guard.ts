39b17858f14386b1b2c70efe0132fc7e8738c8fd
inevitableDivu <pandeydivyansh070501@gmail.com>
2024-04-12 02:26:31 +0530

    Fix: restricting logged in non-admin users to access admin routes

--- server/src/guards/admin.guard.ts
+++ server/src/guards/admin.guard.ts
@@ -1,7 +1,7 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { isJWT } from 'class-validator';
 import { Request } from 'express';
-import { Observable } from 'rxjs';
+import { Observable, retry } from 'rxjs';
 import { HelperService } from 'src/helper/helper.service';
 import { TokenEnum } from 'src/models/helper.models';
 import { User } from 'src/models/user.schema';
@@ -9,47 +9,48 @@ import { AuthService } from 'src/services/auth/auth.service';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
-	constructor(
-		private readonly helper: HelperService,
-		private readonly authService: AuthService,
-	) {}
+    constructor(
+        private readonly helper: HelperService,
+        private readonly authService: AuthService,
+    ) {}
 
-	canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-		const request = context.switchToHttp().getRequest<Request>();
-		const access_token = this.getTokenFromBearerHeader(request);
-		const admin_token = request.cookies[TokenEnum.ADMIN_TOKEN];
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const request = context.switchToHttp().getRequest<Request>();
+        const access_token = this.getTokenFromBearerHeader(request);
+        const admin_token = request.cookies[TokenEnum.ADMIN_TOKEN];
 
-		if (!isJWT(admin_token)) return false;
-		if (!isJWT(access_token)) return false;
+        if (!isJWT(admin_token)) return false;
+        if (!isJWT(access_token)) return false;
 
-		return this.validateAdmin(admin_token, access_token, (user) => {
-			request.user = user;
-		});
-	}
+        return this.validateAdmin(admin_token, access_token, (user) => {
+            request.user = user;
+        });
+    }
 
-	private getTokenFromBearerHeader(request: Request): string {
-		const auth_header = request.headers.authorization;
-		if (!auth_header) return '';
+    private getTokenFromBearerHeader(request: Request): string {
+        const auth_header = request.headers.authorization;
+        if (!auth_header) return '';
 
-		const token = auth_header.split(' ')[1];
-		return token;
-	}
+        const token = auth_header.split(' ')[1];
+        return token;
+    }
 
-	private async validateAdmin(
-		token: string,
-		access_token: string,
-		callback: (user: User) => void,
-	) {
-		const payload = this.helper.decodeToken(TokenEnum.ADMIN_TOKEN, token);
-		const access_token_payload = this.helper.decodeToken(TokenEnum.ACCESS_TOKEN, access_token);
-		if (!payload) return false;
-		if (!access_token_payload) return false;
+    private async validateAdmin(
+        token: string,
+        access_token: string,
+        callback: (user: User) => void,
+    ) {
+        const payload = this.helper.decodeToken(TokenEnum.ADMIN_TOKEN, token);
+        const access_token_payload = this.helper.decodeToken(TokenEnum.ACCESS_TOKEN, access_token);
+        if (!payload) return false;
+        if (!access_token_payload) return false;
+        if (payload.admin_id !== access_token_payload.user_id) return false;
 
-		const user = await this.authService.getUserById(payload.admin_id);
-		if (!user) return false;
+        const user = await this.authService.getUserById(payload.admin_id);
+        if (!user) return false;
 
-		callback(this.authService.serializeUser(user));
+        callback(this.authService.serializeUser(user));
 
-		return true;
-	}
+        return true;
+    }
 }
--- server/src/services/auth/auth.controller.ts
+++ server/src/services/auth/auth.controller.ts
@@ -1,14 +1,14 @@
 import {
-	BadRequestException,
-	Body,
-	ConflictException,
-	Controller,
-	Get,
-	Post,
-	Req,
-	Res,
-	UnprocessableEntityException,
-	UseGuards,
+    BadRequestException,
+    Body,
+    ConflictException,
+    Controller,
+    Get,
+    Post,
+    Req,
+    Res,
+    UnprocessableEntityException,
+    UseGuards,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { ObjectId } from 'mongodb';
@@ -23,110 +23,114 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 @ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
-	constructor(
-		private readonly authService: AuthService,
-		private readonly helper: HelperService,
-	) {}
+    constructor(
+        private readonly authService: AuthService,
+        private readonly helper: HelperService,
+    ) {}
 
-	@Post('register')
-	async handleRegister(
-		@Body() body: CreateUserDto,
-		@Res({ passthrough: true }) response: Response,
-	) {
-		const is_existing_user = await this.authService.getUserByEmail(body.email);
-		if (is_existing_user)
-			throw new ConflictException('Oops! This email is already registered with us.');
+    @Post('register')
+    async handleRegister(
+        @Body() body: CreateUserDto,
+        @Res({ passthrough: true }) response: Response,
+    ) {
+        const is_existing_user = await this.authService.getUserByEmail(body.email);
+        if (is_existing_user)
+            throw new ConflictException('Oops! This email is already registered with us.');
 
-		const user = await this.authService.createUserWithEmailAndPassword(body);
+        const user = await this.authService.createUserWithEmailAndPassword(body);
 
-		if (!user)
-			throw new UnprocessableEntityException(
-				'Oops! Something went wrong while creating your account. Please try again.',
-			);
+        if (!user)
+            throw new UnprocessableEntityException(
+                'Oops! Something went wrong while creating your account. Please try again.',
+            );
 
-		// TODO: trigger user creation event
+        // TODO: trigger user creation event
 
-		const access_token = this.helper.generateToken(TokenEnum.ACCESS_TOKEN, {
-			user_id: user._id,
-			email: user.email,
-		});
+        const access_token = this.helper.generateToken(TokenEnum.ACCESS_TOKEN, {
+            user_id: user._id,
+            email: user.email,
+        });
 
-		// Generate a refresh token and set it as a cookie
-		const token_id = new ObjectId().toString();
-		const refresh_token = this.helper.generateToken(TokenEnum.REFRESH_TOKEN, {
-			token_id,
-			user_id: user._id,
-		});
-		setCookie(response, TokenEnum.REFRESH_TOKEN, refresh_token);
+        // Generate a refresh token and set it as a cookie
+        const token_id = new ObjectId().toString();
+        const refresh_token = this.helper.generateToken(TokenEnum.REFRESH_TOKEN, {
+            token_id,
+            user_id: user._id,
+        });
+        setCookie(response, TokenEnum.REFRESH_TOKEN, refresh_token);
 
-		// TODO: trigger refresh token creation event and store request details with token and it's expiry
+        this.helper.clearCookies(response, TokenEnum.ADMIN_TOKEN);
 
-		return {
-			message: 'Glad to have you onboard! 🎉',
-			user: this.authService.serializeUser(user),
-			token: access_token,
-		};
-	}
+        // TODO: trigger refresh token creation event and store request details with token and it's expiry
 
-	@Post('login')
-	async handleLogin(@Body() body: LoginUserDto, @Res({ passthrough: true }) response: Response) {
-		const user = await this.authService.getUserByEmail(body.email);
-		if (!user)
-			throw new UnprocessableEntityException('Oops! This email is not registered with us.');
+        return {
+            message: 'Glad to have you onboard! 🎉',
+            user: this.authService.serializeUser(user),
+            token: access_token,
+        };
+    }
 
-		const is_password_valid = await this.authService.validatePassword(
-			body.password,
-			user.password,
-		);
-		if (!is_password_valid)
-			throw new BadRequestException(
-				'Um, the password you entered is incorrect. Please try again.',
-			);
+    @Post('login')
+    async handleLogin(@Body() body: LoginUserDto, @Res({ passthrough: true }) response: Response) {
+        const user = await this.authService.getUserByEmail(body.email);
+        if (!user)
+            throw new UnprocessableEntityException('Oops! This email is not registered with us.');
 
-		const access_token = this.helper.generateToken(TokenEnum.ACCESS_TOKEN, {
-			user_id: user._id,
-			email: user.email,
-		});
+        const is_password_valid = await this.authService.validatePassword(
+            body.password,
+            user.password,
+        );
+        if (!is_password_valid)
+            throw new BadRequestException(
+                'Um, the password you entered is incorrect. Please try again.',
+            );
 
-		// Generate a refresh token and set it as a cookie
-		const token_id = new ObjectId().toString();
-		const refresh_token = this.helper.generateToken(TokenEnum.REFRESH_TOKEN, {
-			token_id,
-			user_id: user._id,
-		});
-		setCookie(response, TokenEnum.REFRESH_TOKEN, refresh_token);
+        const access_token = this.helper.generateToken(TokenEnum.ACCESS_TOKEN, {
+            user_id: user._id,
+            email: user.email,
+        });
 
-		// TODO: trigger refresh token creation event and store request details with token and it's expiry
+        // Generate a refresh token and set it as a cookie
+        const token_id = new ObjectId().toString();
+        const refresh_token = this.helper.generateToken(TokenEnum.REFRESH_TOKEN, {
+            token_id,
+            user_id: user._id,
+        });
+        setCookie(response, TokenEnum.REFRESH_TOKEN, refresh_token);
 
-		// TODO: trigger user login event
+        // TODO: trigger refresh token creation event and store request details with token and it's expiry
 
-		const is_admin = user.role === Role.ADMIN;
-		if (is_admin) {
-			// add extra security layer to check for admin in database admin's table
-			const admin_token = this.helper.generateToken(TokenEnum.ADMIN_TOKEN, {
-				admin_id: user._id,
-				role: user.role,
-			});
-			setCookie(response, TokenEnum.ADMIN_TOKEN, admin_token);
-		}
+        // TODO: trigger user login event
 
-		return {
-			message: 'Welcome back buddy! 🎉',
-			user: this.authService.serializeUser(user),
-			token: access_token,
-		};
-	}
+        const is_admin = user.role === Role.ADMIN;
+        if (is_admin) {
+            // add extra security layer to check for admin in database admin's table
+            const admin_token = this.helper.generateToken(TokenEnum.ADMIN_TOKEN, {
+                admin_id: user._id,
+                role: user.role,
+            });
+            setCookie(response, TokenEnum.ADMIN_TOKEN, admin_token);
+        } else {
+            this.helper.clearCookies(response, TokenEnum.ADMIN_TOKEN);
+        }
 
-	@Get('token/refresh')
-	handleTokenRefreshing() {
-		return 'Not Implemented';
-	}
+        return {
+            message: 'Welcome back buddy! 🎉',
+            user: this.authService.serializeUser(user),
+            token: access_token,
+        };
+    }
 
-	@ApiBearerAuth()
-	@UseGuards(AuthGuard)
-	@Get('get/me')
-	async handleGetMe(@Req() request: Request) {
-		if (!request.user) return null;
-		return this.authService.getUserByEmail(request.user.email);
-	}
+    @Get('token/refresh')
+    handleTokenRefreshing() {
+        return 'Not Implemented';
+    }
+
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard)
+    @Get('get/me')
+    async handleGetMe(@Req() request: Request) {
+        if (!request.user) return null;
+        return this.authService.getUserByEmail(request.user.email);
+    }
 }
